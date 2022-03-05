04f1a32c30c4248f8a144881a6369a04d0338835
hisosa <hersonsosa1@gmail.com>
2022-03-05 01:38:35 -0500

    okay i'm done for now

--- src/main/java/com/survivingcodingbootcamp/blog/Populator.java
+++ src/main/java/com/survivingcodingbootcamp/blog/Populator.java
@@ -33,17 +33,19 @@ public class Populator implements CommandLineRunner {
         Topic topic1 = new Topic("Learning TDD");
         topicRepo.save(topic1);
 
-        Hashtag hashtag =  new Hashtag("Computer");
+        Hashtag hashtag =  new Hashtag("thisIsAHashtag");
         hashtagRepository.save(hashtag);
-        Hashtag hashtag1 =  new Hashtag("Mouse");
+        Hashtag hashtag1 =  new Hashtag("HashtagSample");
         hashtagRepository.save(hashtag1);
+        Hashtag hashtag2 = new Hashtag("loveThis!");
+        hashtagRepository.save(hashtag2);
 
         Post post1 = new Post("TDD For Fun and Profit", topic1, "Author 1", "Lorem ipsum dolor sit amet, consectetur " +
                 "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim" +
                 " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis " +
                 "aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
                 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id " +
-                "est laborum.",hashtag);
+                "est laborum.",hashtag,hashtag2);
         postRepo.save(post1);
         Post post2 = new Post("Test the Fear Away", topic1, "Author 2", "Lorem ipsum dolor sit amet, consectetur " +
                 "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim" +
@@ -57,10 +59,18 @@ public class Populator implements CommandLineRunner {
                 " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis " +
                 "aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
                 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id " +
-                "est laborum.",hashtag1);
+                "est laborum.",hashtag1,hashtag2);
         postRepo.save(post3);
+
         Topic topic2 = new Topic("Battling Imposter Syndrome");
         topicRepo.save(topic2);
+        Post post4 = new Post("This is a Title",topic2,"Author4","Lorem ipsum dolor sit amet, consectetur " +
+                "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim" +
+                " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis " +
+                "aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
+                "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id " +
+                "est laborum.",hashtag2);
+        postRepo.save(post4);
         Topic topic3 = new Topic("Introductory Java");
         topicRepo.save(topic3);
         Topic topic4 = new Topic("Object Oriented Programming and You");
